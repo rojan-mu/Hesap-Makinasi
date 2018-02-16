23b8cfe24bc675dc09fc85611769ab2fc171ef2c
omar mustafa <omer1991mustafa@gmail.com>
2018-02-16 16:19:53 +0300

    Add files via upload

---
+++ SharedProject/HesapMakinesiFinal/HesapMakinesi/CodeByMembers.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+        // 1 button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            
+
+        }
+        // + button
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        // 2 button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        // 3 button
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // 4 button
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+        // 5 button
+        private void button5_Click(object sender, EventArgs e)
+        {
+       
+        }
+        // 6 button
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // 7 button
+        private void button9_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // 8 button
+        private void button8_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // 9 button
+        private void button7_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // 0 button
+        private void button10_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // * button
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        // / button
+        private void button13_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // = button
+        private void button15_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+        // Reset button
+        private void button16_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // - button
+        private void button12_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
